fa4a8cf0517930d2e5fac567a0b4c2ea6ac6ed64
emarkula24 <emarkula24@students.oulu.fi>
2025-01-09 14:37:23 +0200

    Organized imports

--- BmiCalculator/app/src/main/java/mies/mummo/bmicalculator/MainActivity.kt
+++ BmiCalculator/app/src/main/java/mies/mummo/bmicalculator/MainActivity.kt
@@ -14,17 +14,17 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.input.KeyboardType
 import mies.mummo.bmicalculator.ui.theme.BmiCalculatorTheme
 import java.text.DecimalFormat
 
